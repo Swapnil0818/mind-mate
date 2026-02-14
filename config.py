c8345a11d3da56eb9a772c7a0fbce85db3a75c22
Swapnil Yadav <123816474+Swapnil0818@users.noreply.github.com>
2026-02-14 20:30:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SESSION_SECRET', 'dev-key-placeholder')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', 'AIzaSyBh0ad5gUxamAF_QtMLjE4hpxP5VDBqGcw')
+    GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', '<API_KEY>')
